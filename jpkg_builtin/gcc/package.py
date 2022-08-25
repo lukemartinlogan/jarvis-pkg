24ee1e1c6abc0c472a3bf18b827ffcbe767204dd
lukemartinlogan <lukemartinlogan@gmail.com>
2022-08-25 05:56:48 -0500

    gcc package

--- jpkg_builtin/gcc/package.py
+++ jpkg_builtin/gcc/package.py
@@ -4,9 +4,11 @@ import os
 
 class Gcc(Package):
     #Distro package managers
-    version('2.0',
-            distro=['debian', 'ubuntu', 'linux-mint'],
-            apt=['gcc', 'g++', 'gfortran', 'build-essential'])
-    version('2.0',
-            distro=['centos7', 'centos8'],
-            yum=['gcc', 'g++', 'gfortran'])
+    def __init__(self):
+        super().__init__()
+        self.version('2.0',
+                distro=['debian', 'ubuntu', 'linux-mint'],
+                apt=['gcc', 'g++', 'gfortran', 'build-essential'])
+        self.version('2.0',
+                distro=['centos7', 'centos8'],
+                yum=['gcc', 'g++', 'gfortran'])
