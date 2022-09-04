4706c2e620e3f6164fdb5279f614bd80cc4dd631
lukemartinlogan <lukemartinlogan@gmail.com>
2022-09-04 16:12:10 -0500

    Ensure that all phases explicitly set their installer

--- jpkg_builtin/io500/package.py
+++ jpkg_builtin/io500/package.py
@@ -12,7 +12,7 @@ class Io500(Package):
                      default=True,
                      msg="Install the DAOS-specific IO500")
 
-    @phase
+    @phase('io500')
     def prepare(self, spec, prefix):
         self.env.set('MY_DAOS_INSTALL_PATH', spec['daos'].prefix)
         self.env.set('MY_MFU_INSTALL_PATH', spec['mfu'].prefix)
@@ -20,7 +20,7 @@ class Io500(Package):
             self.patch('daos')
         ExecNode('./prepare.sh', shell=True).Run()
 
-    @phase
+    @phase('io500')
     def install(self, spec, prefix):
         CopyNode('bin', prefix).run()
         CopyNode('io500', os.path.join(prefix, 'bin')).Run()
